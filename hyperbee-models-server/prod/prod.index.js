f7948d03ee310c26f72f9b48ff0f875856e3369c
yury <yuri.a.samarin@gmail.com>
2025-04-28 11:38:50 +0300

    freeze versions for prod models

---
+++ hyperbee-models-server/prod/prod.index.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const AWS = require('aws-sdk')
+const logger = require('./logger')
+const Hyperswarm = require('hyperswarm')
+const Corestore = require('corestore')
+const Hyperbee = require('hyperbee')
+const { main, cleanUp, handleCleanUp, drives } = require('./prod.app')
+
+const configPath = './prod.config.json'
+const config = require(configPath)
+
+const s3 = new AWS.S3({ region: config.awsRegion })
+const store = new Corestore('./storage')
+const core = store.get({ name: 'hyperbee' })
+const db = new Hyperbee(core, { keyEncoding: 'utf-8', valueEncoding: 'binary' })
+const swarm = new Hyperswarm()
+
+main(configPath, s3, store, db, swarm).then(() => {
+  handleCleanUp(drives, db)
+}).catch(async (err) => {
+  logger.error(`Error in main function: ${err.stack || err}`)
+  await cleanUp(drives, db)
+})
